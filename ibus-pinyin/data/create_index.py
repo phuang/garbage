5fa2e9a19802a86b8eca8e1ed9ce1abfa2ee5c9f
Huang Peng <shawn.p.huang@gmail.com>
2009-03-21 21:28:40 +0800

    WIP.

--- ibus-pinyin/data/create_index.py
+++ ibus-pinyin/data/create_index.py
@@ -6,11 +6,12 @@ con2.execute ("PRAGMA temp_store = MEMORY;")
 
 
 con2.execute("create index index_0_0 on py_phrase_0(s0, y0)")
+print "py_phrase_%d done" % 0
 
 for i in xrange(1, 16):
 	con2.execute("create index index_%d_0 on py_phrase_%d(s0, y0, s1, y1)" % (i, i))
 	con2.execute("create index index_%d_1 on py_phrase_%d(s0, s1, y1)" % (i, i))
-	print "%d done" % i
+	print "py_phrase_%d done" % i
 
 con2.execute("vacuum")
 con2.commit()
