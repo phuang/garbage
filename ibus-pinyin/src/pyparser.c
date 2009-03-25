9e6c6a321d94df0f1f4ecf29c9cc28f9fdc9b6f9
Peng Huang <shawn.p.huang@gmail.com>
2009-03-25 11:31:45 +0800

    WIP.

--- ibus-pinyin/src/pyparser.c
+++ ibus-pinyin/src/pyparser.c
@@ -38,7 +38,7 @@ is_pinyin (const gchar *p,
             return NULL;
         return result;
     }
-    
+
     len = strnlen (p, 6);
     len = MIN (len, end - p);
     strncpy (buf, p, len);
@@ -91,7 +91,7 @@ py_parse_pinyin (const gchar  *str,
 
     if (len < 0)
         len = strlen (str);
-    
+
     array = g_array_sized_new (TRUE, FALSE, sizeof (const PinYin *), len >> 1);
     p = str;
     end = str + len;
@@ -114,19 +114,19 @@ py_parse_pinyin (const gchar  *str,
                     const PinYin **pp;
                     const PinYin *new_py1;
                     const PinYin *new_py2;
-                    
+
                     py = is_pinyin (p, end, -1, option);
 
                     if ((new_py1 = is_pinyin (prev_py->text, prev_py->text + prev_py->len, prev_py->len - 1, option)) != NULL) {
                         new_py2 = is_pinyin (p -1, end, -1, option);
-                        if ((new_py2 != NULL) && (py == NULL || new_py2->len > py->len + 1)) {
+                        if (((new_py2 != NULL) && (new_py2->len > 1 )) && (py == NULL || new_py2->len > py->len + 1)) {
                             g_array_index (array, const PinYin *, array->len - 1) = new_py1;
                             py = new_py2;
                             p --;
                             break;
                         }
                     }
-                    
+
                     if ( py == NULL) {
                         break;
                     }
@@ -146,10 +146,10 @@ py_parse_pinyin (const gchar  *str,
         }
         else {
             py = is_pinyin (p, end, -1, option);
-            if (py == NULL)
-                break;
         }
 
+        if (py == NULL)
+            break;
         g_array_append_val (array, py);
         p += py->len;
 
@@ -170,7 +170,7 @@ py_parse_pinyin (const gchar  *str,
             is_rng = FALSE;
         }
     }
-    
+
     if (p == str) {
         g_array_free (array, TRUE);
         *result = NULL;
@@ -192,18 +192,23 @@ int main(int argc, char **argv)
     gint len;
     GArray *array;
     PinYin **p;
+    gchar *str;
 
-    len = py_parse_pinyin ("qinaide", -1, 0, &array);
+    str = "qinaide";
+
+    if (argc > 1)
+        str = argv[1];
+
+    len = py_parse_pinyin (str, -1, 0xffffffff, &array);
 
     if (len) {
-        
         p = (PinYin **) array->data;
         while (*p) {
             g_printf ("%s'", (*p)->text);
             p ++;
         }
     }
-    g_printf ("\n");
+    g_printf ("%s\n", str + len);
 
     return 0;
 }
