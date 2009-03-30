6d5df60f0b0d4d0c8808928ad1e54ac20509db31
Peng Huang <shawn.p.huang@gmail.com>
2009-03-30 08:45:46 +0800

    Create index on s2, y2.

--- ibus-pinyin/data/create_index.py
+++ ibus-pinyin/data/create_index.py
@@ -8,9 +8,12 @@ con2.execute ("PRAGMA temp_store = MEMORY;")
 con2.execute("create index index_0_0 on py_phrase_0(s0, y0)")
 print "py_phrase_%d done" % 0
 
-for i in xrange(1, 16):
-	con2.execute("create index index_%d_0 on py_phrase_%d(s0, y0, s1, y1)" % (i, i))
-	con2.execute("create index index_%d_1 on py_phrase_%d(s0, s1, y1)" % (i, i))
+con2.execute("create index index_1_0 on py_phrase_1(s0, y0, s1, y1)")
+con2.execute("create index index_1_1 on py_phrase_1(s0, s1, y1)")
+
+for i in xrange(2, 16):
+	con2.execute("create index index_%d_0 on py_phrase_%d(s0, y0, s1, y1, s2, y2)" % (i, i))
+	con2.execute("create index index_%d_1 on py_phrase_%d(s0, s1, s2, y2)" % (i, i))
 	print "py_phrase_%d done" % i
 
 # con2.execute("vacuum")
