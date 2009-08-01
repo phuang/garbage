c6953335e114294b714b01cf5faa9bf618ad7811
Peng Huang <shawn.p.huang@gmail.com>
2009-08-01 11:18:34 +0800

    WIP.

--- taskman/sched/views.py
+++ taskman/sched/views.py
@@ -1,1 +1,18 @@
-# Create your views here.
+from django.template import Context, loader
+from django.http import HttpResponse
+from taskman.sched import models
+
+def expose(template):
+    def __expose(func):
+        def __wrapper(*args, **kargs):
+            t = loader.get_template(template)
+            c = Context(func(*args, **kargs))
+            return HttpResponse(t.render(c))
+        return __wrapper
+    return __expose
+
+@expose(template="sched/tasks.html")
+def tasks(request):
+    tasks = models.Task.objects.all()
+    return { "tasks" : tasks}
+
--- taskman/urls.py
+++ taskman/urls.py
@@ -7,6 +7,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Example:
     # (r'^taskman/', include('taskman.foo.urls')),
+    (r'^sched/', include('taskman.sched.urls')),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
