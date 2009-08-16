4e299218110b85147216cc62786550336c36e971
Peng Huang <shawn.p.huang@gmail.com>
2009-08-16 14:06:09 +0800

    WIP.

--- ST/charset.py
+++ ST/charset.py
@@ -6,7 +6,9 @@ def get_charset(name):
     for l in gzip.open(filename):
         l = l.strip()
         v = l.split()
-        if not v:
+        if len(v) < 3:
+            continue
+        if v[2] != "<CJK>":
             continue
         v = v[0]
         if v.startswith("<U") and v.endswith(">"):
@@ -20,13 +22,19 @@ def get_charset(name):
 
 def main():
     gb2312 = set(get_charset("GB2312"))
+    gbk = set(get_charset("GBK"))
     big5 = set(get_charset("BIG5"))
     print "GB2312", len(gb2312)
+    print "GBK", len(gbk)
     print "BIG5", len(big5)
-    u = gb2312 & big5
-    print "GB2312 & BIG5", len(u)
-    # for c in sorted(list(u)):
-    #     print c
+    u = gb2312 - big5
+    print "GB2312 - BIG5", len(u)
+    
+    for c in sorted(list(u)):
+        print c
+    
+    u = gbk & big5
+    print "GBK & BIG5", len(u)
 
 if __name__ == "__main__":
     main()
