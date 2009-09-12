3a41a00ade940ee5ab5c918849b9b0518b91bae3
Peng Huang <shawn.p.huang@gmail.com>
2009-09-12 16:28:14 +0800

    WIP.

--- ibus-pinyin/src/PhraseEditor.cc
+++ ibus-pinyin/src/PhraseEditor.cc
@@ -2,8 +2,15 @@
 
 namespace PY {
 
+/* init static members */
+Database PhraseEditor::m_database;
+guint PhraseEditor::m_option = 0;
+
 PhraseEditor::PhraseEditor (void)
-    : m_phrases (8)
+    : m_candidates (32),
+      m_phrases (8),
+      m_pinyin (16),
+      m_cursor (0)
 {
 }
 
--- ibus-pinyin/src/PhraseEditor.h
+++ ibus-pinyin/src/PhraseEditor.h
@@ -1,6 +1,7 @@
 #ifndef __PY_PHRASE_EDITOR_H_
 #define __PY_PHRASE_EDITOR_H_
 
+#include "Database.h"
 #include "PhraseArray.h"
 
 namespace PY {
@@ -10,8 +11,35 @@ public:
     PhraseEditor(void);
     ~PhraseEditor(void);
 
+    const PhraseArray & candidates (void) const { return m_candidates; }
+    const PhraseArray & phrases (void) const { return m_phrases; }
+    const PinyinArray & pinyin (void) const { return m_pinyin; }
+    guint cursor (void) const { return m_cursor; }
+
+    const Phrase & candidate (guint i) const {
+        return m_candidates[i];
+    }
+    const Phrase & phrase (guint i) const {
+        return m_phrases[i];
+    }
+
+    void reset (void) {
+        m_candidates.setSize (0);
+        m_phrases.setSize (0);
+        m_cursor = 0;
+    }
+
+    void setPinyin (const PinyinArray &pinyin, gboolean only);
+
 private:
+    PhraseArray m_candidates;
     PhraseArray m_phrases;
+    PinyinArray m_pinyin;
+    guint m_cursor;
+
+private:
+    static Database m_database;
+    static guint m_option;
 };
 
 };
