110e1438906b451b9ea396cbeab75bb304b38f20
Peng Huang <shawn.p.huang@gmail.com>
2009-09-16 20:29:14 +0800

    Add Hostname

--- ibus-pinyin/src/Database.cc
+++ ibus-pinyin/src/Database.cc
@@ -97,7 +97,7 @@ Database::initUserDatabase (const gchar *userdb)
     m_sql << "CREATE TABLE IF NOT EXISTS userdb.desc (name PRIMARY KEY, value TEXT);\n";
     m_sql << "INSERT OR IGNORE INTO userdb.desc VALUES ('version', '1.2.0');\n"
           << "INSERT OR IGNORE INTO userdb.desc VALUES ('uuid', '" << UUID () << "');\n"
-          << "INSERT OR IGNORE INTO userdb.desc VALUES ('hostname', '" << Uname ().hostname () << "');\n"
+          << "INSERT OR IGNORE INTO userdb.desc VALUES ('hostname', '" << Hostname () << "');\n"
           << "INSERT OR IGNORE INTO userdb.desc VALUES ('create-time', datetime ());\n"
           << "INSERT OR IGNORE INTO userdb.desc VALUES ('attach-time', datetime ());\n";
 
