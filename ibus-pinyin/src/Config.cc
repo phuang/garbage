984ec6af27debceb748f58158c7a760134219e01
Peng Huang <shawn.p.huang@gmail.com>
2009-09-19 10:17:46 +0800

    WIP.

--- ibus-pinyin/src/Config.cc
+++ ibus-pinyin/src/Config.cc
@@ -1,7 +1,71 @@
+#include "Types.h"
 #include "Config.h"
+#include "Util.h"
 
 namespace PY {
 
 guint Config::m_option = 0xffffffff;
+
+void
+Config::value_changed_cb (IBusConfig    *config,
+                          const gchar   *section,
+                          const gchar   *name,
+                          const GValue  *value,
+                          Config        *self)
+{
+    static const StaticString engine_pinyin ("engine/PinYin");
+    static const StaticString simple_pinyin ("SimplePinyin");
+    static const struct {
+        StaticString name;
+        guint option;
+        gboolean _default;
+    } options [] = {
+        { StaticString ("SIMPLE_PINYIN"),       PINYIN_SIMPLE_PINYIN,       TRUE },
+        /* correct */
+        { StaticString ("CORRECT_GN_TO_NG"),    PINYIN_CORRECT_GN_TO_NG,    TRUE },
+        { StaticString ("CORRECT_GN_TO_NG"),    PINYIN_CORRECT_GN_TO_NG,    TRUE },
+        { StaticString ("CORRECT_MG_TO_NG"),    PINYIN_CORRECT_MG_TO_NG,    TRUE },
+        { StaticString ("CORRECT_IOU_TO_IU"),   PINYIN_CORRECT_IOU_TO_IU,   TRUE },
+        { StaticString ("CORRECT_UEI_TO_UI"),   PINYIN_CORRECT_UEI_TO_UI,   TRUE },
+        { StaticString ("CORRECT_UEN_TO_UN"),   PINYIN_CORRECT_UEN_TO_UN,   TRUE },
+        { StaticString ("CORRECT_VE_TO_UE"),    PINYIN_CORRECT_VE_TO_UE,    TRUE },
+        /* fuzzy pinyin */
+        { StaticString ("FUZZY_C_CH"),      PINYIN_FUZZY_C_CH,  FALSE },
+        { StaticString ("FUZZY_CH_C"),      PINYIN_FUZZY_CH_C,  FALSE },
+        { StaticString ("FUZZY_Z_ZH"),      PINYIN_FUZZY_Z_ZH,  FALSE },
+        { StaticString ("FUZZY_ZH_Z"),      PINYIN_FUZZY_ZH_Z,  FALSE },
+        { StaticString ("FUZZY_S_SH"),      PINYIN_FUZZY_S_SH,  FALSE },
+        { StaticString ("FUZZY_SH_S"),      PINYIN_FUZZY_SH_S,  FALSE },
+        { StaticString ("FUZZY_L_N"),       PINYIN_FUZZY_L_N,   FALSE },
+        { StaticString ("FUZZY_N_L"),       PINYIN_FUZZY_N_L,   FALSE },
+        { StaticString ("FUZZY_F_H"),       PINYIN_FUZZY_F_H,   FALSE },
+        { StaticString ("FUZZY_H_F"),       PINYIN_FUZZY_H_F,   FALSE },
+        { StaticString ("FUZZY_L_R"),       PINYIN_FUZZY_L_R,   FALSE },
+        { StaticString ("FUZZY_R_L"),       PINYIN_FUZZY_R_L,   FALSE },
+        { StaticString ("FUZZY_K_G"),       PINYIN_FUZZY_K_G,   FALSE },
+        { StaticString ("FUZZY_G_K"),       PINYIN_FUZZY_G_K,   FALSE },
+        { StaticString ("FUZZY_AN_ANG"),    PINYIN_FUZZY_AN_ANG,    FALSE },
+        { StaticString ("FUZZY_ANG_AN"),    PINYIN_FUZZY_ANG_AN,    FALSE },
+        { StaticString ("FUZZY_EN_ENG"),    PINYIN_FUZZY_EN_ENG,    FALSE },
+        { StaticString ("FUZZY_ENG_EN"),    PINYIN_FUZZY_ENG_EN,    FALSE },
+        { StaticString ("FUZZY_IN_ING"),    PINYIN_FUZZY_IN_ING,    FALSE },
+        { StaticString ("FUZZY_ING_IN"),    PINYIN_FUZZY_ING_IN,    FALSE },
+        { StaticString ("FUZZY_UAN_UANG"),  PINYIN_FUZZY_UAN_UANG,  FALSE },
+        { StaticString ("FUZZY_UANG_UAN"),  PINYIN_FUZZY_UANG_UAN,  FALSE },
+    };
+
+    for (guint i = 0;i < sizeof (options) / sizeof (options[0]); i++) {
+        if (G_UNLIKELY (options[i].name == name)) {
+            gboolean v = options[i]._default;
+            if (G_LIKELY (value != NULL || G_VALUE_TYPE (value) == G_TYPE_BOOLEAN))
+                v = g_value_get_boolean (value);
+            if (v)
+                m_option |= options[i].option;
+            else
+                m_option ^= options[i].option;
+        }
+    }
+}
+
 
 };
--- ibus-pinyin/src/Config.h
+++ ibus-pinyin/src/Config.h
@@ -2,17 +2,31 @@
 #define __PY_CONFIG_H_
 
 #include <glib.h>
+#include <glib-object.h>
 #include <ibus.h>
 #include "Pointer.h"
 
 namespace PY {
 
-class Config : public Pointer<IBusConfig > {
+class Config {
 public:
     Config (Pointer<IBusBus> & bus) {
-        set (ibus_bus_get_config (bus));
+        m_config = ibus_bus_get_config (bus);
+        g_signal_connect ((IBusConfig *) m_config, "value-changed", G_CALLBACK (value_changed_cb), this);
     }
+
     static guint option (void) { return m_option; }
+
+private:
+    static void value_changed_cb (IBusConfig    *config,
+                                  const gchar   *section,
+                                  const gchar   *name,
+                                  const GValue  *value,
+                                  Config        *self);
+
+private:
+    Pointer<IBusConfig> m_config;
+
 private:
     static guint m_option;
 };
