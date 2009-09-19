afdbcadbca5c7fa457a3076624ed63c9ce0ef8d7
Peng Huang <shawn.p.huang@gmail.com>
2009-09-19 22:16:59 +0800

    WIP.

--- ibus-pinyin/setup/main.py
+++ ibus-pinyin/setup/main.py
@@ -1,6 +1,19 @@
 import gtk
+import ibus
 
-b = gtk.Builder()
-b.add_from_file("ibus-pinyin-preferences.glade")
-d = b.get_object("dialog");
-d.run()
+class PreferencesDialog:
+    def __init__(self):
+        self.__bus = ibus.Bus()
+        self.__builder = gtk.Builder()
+        self.__builder.add_from_file("ibus-pinyin-preferences.glade")
+        self.__dialog = self.__builder.get_object("dialog")
+
+    def run(self):
+        return self.__dialog.run()
+
+def main():
+    PreferencesDialog().run()
+
+
+if __name__ == "__main__":
+    main()
