b0ed7599fb88d45562bf3e23684d3d9c9c6a9826
Peng Huang <shawn.p.huang@gmail.com>
2009-09-20 12:43:55 +0800

    WIP.

--- ibus-pinyin/setup/main.py
+++ ibus-pinyin/setup/main.py
@@ -73,37 +73,39 @@ class PreferencesDialog:
         self.__correct_pinyin.connect("toggled", __correct_pinyin_toggled_cb)
 
         # fuzzy pinyin
-        self.__fuzzy = self.__builder.get_object("checkbuttonFuzzy")
-        self.__fuzzy_c_ch = self.__builder.get_object("checkbuttonFuzzyC_CH")
-        self.__fuzzy_z_zh = self.__builder.get_object("checkbuttonFuzzyZ_ZH")
-        self.__fuzzy_s_sh = self.__builder.get_object("checkbuttonFuzzyS_SH")
-        self.__fuzzy_ch_c = self.__builder.get_object("checkbuttonFuzzyCH_C")
-        self.__fuzzy_zh_z = self.__builder.get_object("checkbuttonFuzzyZH_Z")
-        self.__fuzzy_sh_s = self.__builder.get_object("checkbuttonFuzzySH_S")
-        
-        self.__fuzzy_l_n = self.__builder.get_object("checkbuttonFuzzyL_N")
-        self.__fuzzy_f_h = self.__builder.get_object("checkbuttonFuzzyF_H")
-        self.__fuzzy_l_r = self.__builder.get_object("checkbuttonFuzzyL_R")
-        self.__fuzzy_k_g = self.__builder.get_object("checkbuttonFuzzyK_G")
-        self.__fuzzy_n_l= self.__builder.get_object("checkbuttonFuzzyN_L")
-        self.__fuzzy_h_f= self.__builder.get_object("checkbuttonFuzzyH_F")
-        self.__fuzzy_r_l= self.__builder.get_object("checkbuttonFuzzyR_L")
-        self.__fuzzy_g_k= self.__builder.get_object("checkbuttonFuzzyG_K")
-        
-        self.__fuzzy_an_ang = self.__builder.get_object("checkbuttonFuzzyAN_ANG")
-        self.__fuzzy_en_eng = self.__builder.get_object("checkbuttonFuzzyEN_ENG")
-        self.__fuzzy_in_ing = self.__builder.get_object("checkbuttonFuzzyIN_ING")
-        self.__fuzzy_ang_an = self.__builder.get_object("checkbuttonFuzzyANG_AN")
-        self.__fuzzy_eng_en = self.__builder.get_object("checkbuttonFuzzyENG_EN")
-        self.__fuzzy_ing_in = self.__builder.get_object("checkbuttonFuzzyING_IN")
-        
-        self.__fuzzy_ian_iang = self.__builder.get_object("checkbuttonFuzzyIAN_IANG")
-        self.__fuzzy_uan_uang = self.__builder.get_object("checkbuttonFuzzyUAN_UANG")
-        self.__fuzzy_iang_ian = self.__builder.get_object("checkbuttonFuzzyIANG_IAN")
-        self.__fuzzy_uang_uan = self.__builder.get_object("checkbuttonFuzzyUANG_UAN")
+        self.__fuzzy_pinyin = self.__builder.get_object("FuzzyPinyin")
+        self.__fuzzy_pinyin_widgets = [
+            "FuzzyPinyinC_CH",
+            "FuzzyPinyinC_CH",
+            "FuzzyPinyinZ_ZH",
+            "FuzzyPinyinS_SH",
+            "FuzzyPinyinCH_C",
+            "FuzzyPinyinZH_Z",
+            "FuzzyPinyinSH_S",
+            "FuzzyPinyinL_N",
+            "FuzzyPinyinF_H",
+            "FuzzyPinyinL_R",
+            "FuzzyPinyinK_G",
+            "FuzzyPinyinN_L",
+            "FuzzyPinyinH_F",
+            "FuzzyPinyinR_L",
+            "FuzzyPinyinG_K",
+            "FuzzyPinyinAN_ANG",
+            "FuzzyPinyinEN_ENG",
+            "FuzzyPinyinIN_ING",
+            "FuzzyPinyinANG_AN",
+            "FuzzyPinyinENG_EN",
+            "FuzzyPinyinING_IN",
+            "FuzzyPinyinIAN_IANG",
+            "FuzzyPinyinUAN_UANG",
+            "FuzzyPinyinIANG_IAN",
+            "FuzzyPinyinUANG_UAN"]
 
-        def __fuzzy_toggled_cb(w):
+        def __fuzzy_pinyin_toggled_cb(w):
             v = w.get_active()
+            for name in self.__fuzzy_pinyin_widgets:
+                self.__builder.get_object(name).set_sensitive(v)
+        self.__fuzzy_pinyin.connect("toggled", __fuzzy_pinyin_toggled_cb)
 
 
 
