e9b9e534490c81db6a0c2bc1a83e360a2f00c611
Peng Huang <shawn.p.huang@gmail.com>
2010-01-14 10:41:10 +0800

    .

--- gtk/label.py
+++ gtk/label.py
@@ -1,12 +1,29 @@
 import gtk
+import pango
+import glib
+import random
+
+def clicked_cb(button, label):
+    label.set_attributes(pango.AttrList())
 
 def main():
-	w = gtk.Window()
-	l = gtk.Label("Hello World")
-	l.set_property("cursor-position", 3)
-	w.add(l)
-	w.show_all()
-	gtk.main();
+    global l
+    window = gtk.Window()
+    vbox=gtk.VBox()
+    window.add(vbox)
+    
+    label = gtk.Label("Hello World")
+    attr = pango.AttrList()
+    attr.insert(pango.AttrBackground(0, 255*255, 0, 0, 11))
+    label.set_attributes(attr)
+    vbox.pack_start(label)
+
+    button = gtk.Button("Change Attributes")
+    button.connect("clicked", clicked_cb, label)
+    vbox.pack_start(button)
+    
+    window.show_all()
+    gtk.main();
 
 if __name__ == "__main__":
-	main()
+    main()
